19674f55012448266c0d379fb32932e8efc85387
nada <nada@mlata>
2017-05-16 23:37:47 +0200

    added 'start' log in HelloGit class

--- GitHelloWorld/src/com/git/test/HelloGit.java
+++ GitHelloWorld/src/com/git/test/HelloGit.java
@@ -3,6 +3,7 @@ package com.git.test;
 public class HelloGit {
 
 	public static void main(String[] args) {
+		System.out.println("=== START ===");
 		System.out.println("Hello Git");
 		Test.printText();
 	}
