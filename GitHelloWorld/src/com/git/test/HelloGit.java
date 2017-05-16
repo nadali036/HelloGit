c020f7ccd201b8e20ddd588e8fcd1d76b8483804
nada <nada@mlata>
2017-05-16 23:38:34 +0200

    updated HelloGit and Test classes

--- GitHelloWorld/src/com/git/test/HelloGit.java
+++ GitHelloWorld/src/com/git/test/HelloGit.java
@@ -6,5 +6,6 @@ public class HelloGit {
 		System.out.println("=== START ===");
 		System.out.println("Hello Git");
 		Test.printText();
+		System.out.println("=== END ===");
 	}
 }
--- GitHelloWorld/src/com/git/test/Test.java
+++ GitHelloWorld/src/com/git/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	
 	public static void printText() {
 		System.out.println("In Test class method printText");
-		System.out.println("Added new message");
+		System.out.println("**** Added new message ****");
 		System.out.println("Added one more message");
 	}
 }
