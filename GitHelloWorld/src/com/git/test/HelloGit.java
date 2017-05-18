9aa5f0affbe3c9e0a6eeb17ff9655ea566472f26
nadali036 <nadali036@gmail.com>
2017-05-18 22:55:18 +0200

    updated start msg in HelloGit

--- GitHelloWorld/src/com/git/test/HelloGit.java
+++ GitHelloWorld/src/com/git/test/HelloGit.java
@@ -3,7 +3,7 @@ package com.git.test;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("=== START ===");
+		System.out.println("===### START ###===");
 		System.out.println("Hello Git");
 		Test.printText();
 		System.out.println("=== end ===");
