57e58e228670f8b2eb8e6c452dd1cf5768843908
nadali036 <nada@mlata>
2017-05-19 17:54:51 +0200

    removed msg's from HelloGit class

--- GitHelloWorld/src/com/git/test/HelloGit.java
+++ GitHelloWorld/src/com/git/test/HelloGit.java
@@ -5,8 +5,6 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("=== start ===");
 		System.out.println("Hello Git!!!");
-		System.out.println("Hello Git!!!");
-		System.out.println("Hello Git!!!");
 		Test.printText();
 		System.out.println("=== end ===");
 	}
