ee61c03e8115ee7d0a6560377cd000b8e9fa815f
nadali036 <nada@mlata>
2017-05-19 17:52:47 +0200

    added one more msg to HelloGit

--- GitHelloWorld/src/com/git/test/HelloGit.java
+++ GitHelloWorld/src/com/git/test/HelloGit.java
@@ -6,6 +6,7 @@ public class HelloGit {
 		System.out.println("=== start ===");
 		System.out.println("Hello Git!!!");
 		System.out.println("Hello Git!!!");
+		System.out.println("Hello Git!!!");
 		Test.printText();
 		System.out.println("=== end ===");
 	}
